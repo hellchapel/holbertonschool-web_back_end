dbc70d25c25b44793ad7a5b288ca7223b5bcc3cf
hellchapel <hellchapel@example.com>
2026-04-28 13:43:40 -0600

    Add task 14: top students - 101-students.py

---
+++ NoSQL/101-students.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module that returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": 1,
+                "topics": 1,
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ])
